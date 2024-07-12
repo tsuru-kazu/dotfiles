8bf8bbf9153eab67bcc64e90eaa8f5a72efac3fa
tsurusekazuki <kazuki.tsuruse@geniee.co.jp>
2024-07-12 23:26:21 +0900

    add: Taskfileの導入

--- brew.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Install Homebrew if it's not already installed
-if ! command -v brew &> /dev/null; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-# Install packages from Brewfile
-brew bundle --file=~/dotfiles/Brewfile
