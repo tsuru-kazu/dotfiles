f20f32dc90d1024c6745a3d4041a2b6f1f3ca4b6
tsurusekazuki <kazuki.tsuruse@geniee.co.jp>
2024-07-31 19:31:42 +0900

    update: brewfile

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,6 @@
 alias g "git"
 alias lzd 'lazydocker'
 alias gl "cd (ghq root)/(ghq list | peco)"
-alias gh "hub browse (ghq list | peco | cut -d "/" -f 2,3)"
 
 set -x BREW_HOME /home/linuxbrew/.linuxbrew/bin
 set -x PATH $PATH $BREW_HOME
