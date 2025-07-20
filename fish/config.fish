432e31fb10d3676eeb8a5395266facc328ea360f
tsuru-kazu <kz.nuoun1012@gmail.com>
2025-07-20 11:10:43 +0900

    add: tmux alias

--- fish/config.fish
+++ fish/config.fish
@@ -8,15 +8,4 @@ set -x PATH $PATH $BREW_HOME
 set -x PATH $HOME/.anyenv/bin $PATH
 eval (anyenv init - | source)
 
-# set -x GOPROXY direct
-# set -x GOPATH $HOME/go
-# set -x GOROOT $HOME/.anyenv/envs/goenv/versions/1.21.1
-# set -x PATH $PATH $GOROOT/bin
-# set -x PATH $PATH $GOPATH/bin
-
-# グローバルnpm
-# set -x PATH $HOME/.anyenv/envs/nodenv/versions/18.18.0/bin $PATH
-
-# set -x OPENAI_API_KEY sk-xxxxxx
-
-# source ~/.asdf/asdf.fish
+alias t='tmux new-session \; split-window -v \; split-window -h \; select-pane -t 0 \; split-window -h \; select-pane -t 0'
