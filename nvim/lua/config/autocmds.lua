a706fd9950e22aec098da7cc974df1a3f351f01d
tsuru-kazu <kz.nuoun1012@gmail.com>
2025-08-11 20:31:25 +0900

    自動保存の設定を追加

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -76,3 +76,12 @@ vim.api.nvim_create_autocmd({ "VimResized" }, {
     vim.cmd("tabnext " .. current_tab)
   end,
 })
+
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  group = augroup("auto_save"),
+  callback = function(event)
+    if vim.bo[event.buf].modified and vim.bo[event.buf].buftype == "" and vim.fn.expand("%") ~= "" then
+      vim.cmd("silent! write")
+    end
+  end,
+})
