ddcd3532c77fe2b6cdbc1d29aa7ade9faada5078
tsuru-kazu <kz.nuoun1012@gmail.com>
2025-08-13 22:07:32 +0900

    add: lazygitの導入

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 alias g "git"
 alias lzd 'lazydocker'
 alias gl "cd (ghq root)/(ghq list | peco)"
+alias lg "lazygit"
 
 set -x BREW_HOME /home/linuxbrew/.linuxbrew/bin
 set -x PATH $PATH $BREW_HOME
