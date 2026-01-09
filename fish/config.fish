c3b995658989c1373a6437ec492f9733614ffa81
tsuru-kazu <kz.nuoun1012@gmail.com>
2026-01-09 21:58:17 +0900

    add

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,7 @@ alias lzd 'lazydocker'
 alias gl "cd (ghq root)/(ghq list | peco)"
 alias lg "lazygit"
 alias vi "nvim"
+alias cs "cursor ."
 
 set -x BREW_HOME /home/linuxbrew/.linuxbrew/bin
 set -x PATH $PATH $BREW_HOME
